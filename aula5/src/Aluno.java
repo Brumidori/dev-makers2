7d65c07ce8997dd9ae02b99a1a6414d182b1ea1d
Bruna_Midori <brunamidoriyy@yahoo.com.br>
2022-11-21 22:16:39 -0300

    acrescimos exercicio objeto

--- aula5/src/Aluno.java
+++ aula5/src/Aluno.java
@@ -1,10 +1,7 @@
 public class Aluno {
 
-    public int matricula;
-    public double n1;
-    public double n2;
-    public double n3;
-    public double n4;
+    private int matricula;
+    double [] notas = new double[4];
 
     public int getMatricula() {
         return matricula;
@@ -14,40 +11,14 @@ public class Aluno {
         this.matricula = matricula;
     }
 
-    public double getN1() {
-        return n1;
-    }
-
-    public void setN1(double n1) {
-        this.n1 = n1;
-    }
-
-    public double getN2() {
-        return n2;
-    }
-
-    public void setN2(double n2) {
-        this.n2 = n2;
-    }
-
-    public double getN3() {
-        return n3;
-    }
-
-    public void setN3(double n3) {
-        this.n3 = n3;
-    }
-
-    public double getN4() {
-        return n4;
-    }
-
-    public void setN4(double n4) {
-        this.n4 = n4;
-    }
 
     public static double media(Aluno a){
-        return (a.getN1() + a.getN2() + a.getN3() + a.getN4())/4f;
+        double soma= 0;
+        for (double nota: a.notas
+             ) {
+            soma += nota;
+        }
+        return (soma/4);
 
     }
 
--- aula5/src/TesteAluno.java
+++ aula5/src/TesteAluno.java
@@ -4,39 +4,39 @@ public class TesteAluno {
 
     public static void main(String[] args) {
         Aluno ana = new Aluno();
-        ana.setN1(9);
-        ana.setN2(3);
-        ana.setN3(8);
-        ana.setN4(7);
+        ana.notas[0] = 9.0;
+        ana.notas[1] = 7.0;
+        ana.notas[2] = 8.0;
+        ana.notas[3] = 9.0;
         ana.setMatricula(1);
 
         Aluno breno = new Aluno();
-        breno.setN1(1);
-        breno.setN2(10);
-        breno.setN3(6);
-        breno.setN4(8);
+        breno.notas[0] = 9.0;
+        breno.notas[1] = 4.0;
+        breno.notas[2] = 8.0;
+        breno.notas[3] = 5.0;
         breno.setMatricula(2);
 
 
         Aluno clara = new Aluno();
-        clara.setN1(1);
-        clara.setN2(2);
-        clara.setN3(3);
-        clara.setN4(4);
+        clara.notas[1] = 7.0;
+        clara.notas[2] = 4.0;
+        clara.notas[3] = 3.0;
+        clara.notas[0] = 1.0;
         clara.setMatricula(3);
 
         Aluno dani = new Aluno();
-        dani.setN1(1);
-        dani.setN2(4);
-        dani.setN3(5);
-        dani.setN4(9);
+        dani.notas[0] = 9.0;
+        dani.notas[1] = 3.0;
+        dani.notas[2] = 4.0;
+        dani.notas[3] = 0.0;
         dani.setMatricula(4);
 
         Aluno elise = new Aluno();
-        elise.setN1(9);
-        elise.setN2(7);
-        elise.setN3(8);
-        elise.setN4(9);
+        elise.notas[0] = 9.0;
+        elise.notas[1] = 8.0;
+        elise.notas[2] = 6.0;
+        elise.notas[3] = 5.0;
         elise.setMatricula(5);
 
         ArrayList<Aluno> turma = new ArrayList<Aluno>();
@@ -47,12 +47,17 @@ public class TesteAluno {
         turma.add(elise);
 
         for (Aluno a : turma) {
-            System.out.println("Aluno: " + a.getMatricula() + ";\nNotas: " + a.getN1() + ", " + a.getN2() + ", " + a.getN3() + ", " + a.getN4());}
+            System.out.println("Aluno: " + a.getMatricula());
+            for (double nota : a.notas
+            ) {
+                System.out.println("Notas: " + nota);
+            }
+        }
 
-        System.out.println("----------------------------------------------------------------");
+            System.out.println("----------------------------------------------------------------");
 
-        for (Aluno a : turma) {
-            System.out.println("Aluno: " + a.getMatricula() + ";\nMédia: " + Aluno.media(a));
+            for (Aluno a : turma) {
+                System.out.println("Aluno: " + a.getMatricula() + ";\nMédia: " + Aluno.media(a));
+            }
         }
     }
-}
